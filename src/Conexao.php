975a0aa40a1beabdf274bc9318db713c1bdef1a2
Sebastião <swillian2009@gmail.com>
2022-05-31 19:44:54 -0300

    Implementação da classe conexão com a base de dados.

---
+++ src/Conexao.php
@@ -0,0 +1,27 @@
+<?php
+    /**
+    * Classe responsável com a conexão com o banco de dados.
+    */ 
+    class Conexao
+    {
+        private $servername = 'localhost:3306';
+        private $username = 'root';
+        private $password = 'HORTETEC_115';
+        private $database = 'biblioteca';
+        private $conection;
+
+        /**
+        * Realiza a conexão com a base de dados.
+        */ 
+        public function getConection()
+        {
+            if(is_null($this->conection))
+            {
+                $this->conection = new PDO('mysql:host='.$this->servername.';dbname='.$this->database, $this->username,$this->password);
+                $this->conection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                $this->conection->exec('set names utf8');
+            }
+            return $this->conection;
+        }
+    }
+?>
