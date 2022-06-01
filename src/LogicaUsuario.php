b97503d1057cfed14bf4de44a7acb37566887831
Sebastião <swillian2009@gmail.com>
2022-05-31 22:38:13 -0300

    Implementação da lógica de usuário.

---
+++ src/LogicaUsuario.php
@@ -0,0 +1,50 @@
+<?php
+	session_start();
+
+	/**
+    * Classe responsável pela persistência de dados dentro da aplicação.
+    */ 
+	function usuarioEstaLogado() 
+	{
+		return isset($_SESSION['usuario_logado']);
+	}
+
+	/**
+    * Função que verifica o nível de permissão do usuário para acesso as funcionalidades.
+    */ 
+	function verificaUsuario() 
+	{
+		if(!usuarioEstaLogado()) 
+		{
+			$_SESSION['danger'] = "Você não tem acesso à essa funcionalidade!";
+			header("Location: index.php");
+			die();
+		}
+	}
+
+	/**
+    * Função responsável por manter o usuário logado.
+    */ 
+	function usuarioLogado() 
+	{
+		return $_SESSION['usuario_logado'];
+	}
+
+	/**
+    * Função responsável por logar um usuário com base em seu email cadastrado.
+    * @param mixed $email Email cadastrado na base de dadps para login.
+	*/ 
+	function logaUsuario($email) 
+	{
+		$_SESSION['usuario_logado'] = $email;
+	}
+
+	/**
+    * Função responsável por deslogar um usuário logado.
+    */ 
+	function logout() 
+	{
+		session_destroy();
+		session_start();
+	}
+?>
