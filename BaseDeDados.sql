fefba830e3f5024ad0dd2cad586439131c5ea8d6
Sebastião <swillian2009@gmail.com>
2022-05-31 21:49:10 -0300

    Base de dados SQL está testada e funcionando.

---
+++ BaseDeDados.sql
@@ -0,0 +1,28 @@
+create database livraria;
+use livraria;
+
+create table livro(
+    id int primary key auto_increment primary key,
+    nomeLivro varchar(100) not null,
+    nomeAutor varchar(100) not null,
+    fotoCapa varchar(30),
+    ISBM varchar(17),
+    editora varchar(30) not null
+);
+
+create table usuario(
+    id int auto_increment primary key,
+    login varchar(10),
+    senha varchar(100),
+    permissao char(1)
+);
+
+create table visualizacao(
+    id int auto_increment primary key,
+    idUsuario  int,
+    idLivro int,
+    data date,
+    hora time,
+    foreign key(idUsuario) references usuario(id),
+    foreign key(idLivro) references livro(id)
+);
