8e6a7fe920fa4997ecf06edbb5d9fef84eaedfd5
Sebastião <swillian2009@gmail.com>
2022-06-21 22:34:34 -0300

    Alteração na base de dados.

--- BaseDeDados.sql
+++ BaseDeDados.sql
@@ -1,6 +1,8 @@
-create database livraria;
-use livraria;
-
+-- Criar o banco de dados.
+create database biblioteca;
+-- Usar o banco de dados.
+use biblioteca;
+-- Criar a tabela com os suas propriedades.
 create table livro(
     id int primary key auto_increment primary key,
     nomeLivro varchar(100) not null,
@@ -9,20 +11,26 @@ create table livro(
     ISBM varchar(17),
     editora varchar(30) not null
 );
-
+-- Criar a tabela com os suas propriedades.
 create table usuario(
     id int auto_increment primary key,
-    login varchar(10),
-    senha varchar(100),
-    permissao char(1)
+    login varchar(10) not null,
+    senha varchar(100) not null,
+    permissao char(1) not null
 );
 
-create table visualizacao(
+-- #IMPORTANTE
+-- Um super usuário vai ser cadastrado via query sql para viabilizar a navegação inicial no projeto.
+
+-- Criar a tabela com os suas propriedades.
+create table emprestimo(
     id int auto_increment primary key,
     idUsuario  int,
     idLivro int,
-    data date,
-    hora time,
+    dataInicial TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP, -- Insere da data do ato do empréstimo. Verificar na base como esse tipo de dado se comporta.
+	dataFinal date,
     foreign key(idUsuario) references usuario(id),
     foreign key(idLivro) references livro(id)
 );
+-- Descrição das colunas de uma tabela.
+show COLUMNS from <nomeDaTabela>;
