d4b285e532c316a3bc36a3c9f20fe4ff3c16a241
Sebastião <swillian2009@gmail.com>
2022-06-21 22:35:17 -0300

    Iniciada a implementação da model da Biblioteca.

---
+++ src/model/Biblioteca.php
@@ -0,0 +1,9 @@
+<?php
+    /**
+    * Objeto de valor que representa uma biblioteca.
+    */ 
+    class Biblioteca
+    {
+        // Responsável por empréstimos e operações com usuários.
+    }
+?>
