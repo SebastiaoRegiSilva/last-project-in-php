5e0e756af690bfd063447d32eb86cbfc2047c530
Sebastião Willian <swillian2009@gmail.com>
2022-06-26 23:55:15 -0300

    Implementação dos cadastros.

--- src/view/cadUsuario.php
+++ src/view/cadUsuario.php
@@ -1,81 +1,114 @@
-<?php 
-    require_once("Cabecalho.php"); 
-	require_once("Conexao.php");
-	require_once("banco-livraria.php");
-	require_once("logica-usuario.php");
-	require_once("logica-empresta.php");
+<!DOCTYPE html>
+<html lang="pt-BR">
 
-	if(empty($_SESSION['emprestimo']))
-    { 
-?>
-		<div class="alert alert-success" role="alert">Lista de empréstimo vazia.</div>
-<?php 
-    }
-    else
-    { 
-?>
-    <?php if(usuarioEstaLogado()){ ?>
-	<h2>Busque o usuário pelo ID</h2>
-	<form action="busca-usuario.php" method="post" class="form-horizontal">
-		<div class="form-group">
-			<label for="inputName" class="col-sm-2 control-label">Digite o ID do Usuário.</label>
-			<div class="col-sm-10">
-				<input type="text" class="form-control" id="inputName" name="id" placeholder="Digite o ID do usuário">
-			</div>
-		</div>
-		<br/>
-		<div class="form-group">
-			<div class="col-sm-offset-2 col-sm-10">
-				<button type="submit" class="btn btn-default">Buscar Usuário</button>
-			</div>
-		</div> 
-	</form>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/loja.css" rel="stylesheet">
+	<title>Cadastro de Usuário.</title>
+</head>
 
-	<?php 
-		if(isset($_SESSION['id_usuario'])){
-			$usuario = buscaUsuario($conexao, $_SESSION['id_usuario']); ?>
-			<p class="lead">Cliente <?=$usuario['nome']?></p>
-	<?php	
-        } 
-    ?>
+<body>
+	<?php
+		require_once('C:\xampp\htdocs\last-project-in-php\src\Cabecalho.php');
+		
+		if (!isset($_SESSION['userLogin']) == true) {
+			unset($_SESSION['Login.php']);
+			header('Location: Login.php');
+		}
+	
+		$logado = $_SESSION['userLogin'];
+		
+		if ($logado->getPermissao() == 'A') 
+		{ ?>
+			<div class="container-fluid mt--7">
+				<div class="row">
+					<div class="col">
+						<div class="card shadow">
+							<div class="card-header border-1">
+								<h3 class="mb-0">Cadastro de Usuário.</h3>
+							</div>
 
-	<table class="table table-bordered table-striped">
-		<tr>
-			<td>Nome:</td>
-			<td>Sinopse</td>
-		</tr>
-		<tr>
-			<?php 
+							<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+							<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
 
-
-			foreach($_SESSION['emprestimo'] as $livro) 
-            { 
-				$resultado = buscaLivro($conexao, $livro)
-
-				?>
-				<td><?=$resultado['nome']?></td>
-				<td><?=$resultado['sinopse']?></td>
-				
-					
-					<td><a href="excluir-emprestimo.php?id=<?=$resultado['id']?>" class="btn btn-danger">Excluir</a></td>
-				</tr>
-        <?php 
-            } 
-        ?>
-	</table>
-
-    <?php 
-        }
-        else
-        { 
-    ?>
-        <div class="alert alert-danger" role="alert">Para usar esta funcionalidade você precisa estar logado.</div>
-    <?php 
-        } 
-    ?>
-    <a href="emprestimo.php" class="btn btn-default">Emprestar</a>
-    <a href="cancela-emprestimo.php" class="btn btn-danger">Cancelar empréstimo</a>
-<?php 
-}
-    require_once("rodape.php"); 
-?>
+							<div class="container" style="margin-top: 10px;">
+								<form name="cadUsuario" action="" method="POST">
+									<div class="col-md-5">
+										<form name="cadUsuario" action="" method="POST">
+										<!-- Login. -->
+											<input type="text" name="id" id="id" value="<?php echo isset($usuario) ? $usuario->getId() : '' ?>" hidden="" />
+											<div class="mb-3">
+												<label class="form-label">Login:</label>
+												<input type="text" class="form-control" name="login" value="" ?>
+											</div>
+											<br>
+											<!-- Senha. -->
+											<div class="mb-3">
+                       						 <label class="form-label">Senha:</label>
+                        						<input type="password" class="form-control" name="senha1" value=""?>
+                   							 </div>
+											<br>
+											<!-- Confirmar Senha. -->
+											<div class="mb-3">
+												<label class="form-label">Confirmar senha:</label>
+												<input type="password" class="form-control" name="senha2">
+											</div>
+											<br>
+											<!-- Permissão. -->
+											<div class="mb-3">
+												<label class="form-label">Permissão:</label>
+												<select class="form-select" name="permissao">
+													<option value="A" <?php echo isset($usuario) && $usuario->getPermissao() == 'A' ? 'seleted' : '' ?>>Administrador</option>
+													<option value="C" <?php echo isset($usuario) && $usuario->getPermissao() == 'C' ? 'seleted' : '' ?>>Comum</option>
+												</select>
+											</div>
+											<br>
+										</div>
+									<div class="card-footer py-4">
+										<div style="text-align: left">
+											<button type="submit" name="btnSalvar" class="btn btn-primary">Cadastrar</button>
+										</div>
+									</div>
+								</form>
+								<br>
+								<?php
+									if(isset($_POST['btnSalvar'])){
+									$salvar = call_user_func(array('UsuarioController', 'salvar'));
+										if($salvar)
+										{
+								?>
+											<div class="alert alert-success" role="alert">
+												Usuário cadastrado com sucesso!!!
+											</div>
+									<?php 
+										}
+										else
+										{
+									?>
+										<div class="alert alert-danger" role="alert">
+												Senhas não são iguais!
+											</div>
+									<?php
+										}
+									}   
+									?>
+							</div>
+						</div>
+					</div>
+				</div>
+			<?php } ?>
+			
+			<?php
+			if ($logado->getPermissao() != 'A') 
+			{ ?>
+				<h3 class="mb-0">HAHA, VOCÊ NÃO PODE FICA AQUI VOLTE!</h3>
+			<?php
+				// Redireciona para a index.
+				header('Location: C:\xampp\htdocs\last-project-in-php\src\Index.php');
+			} 
+	?>
+</body>
+</html>
