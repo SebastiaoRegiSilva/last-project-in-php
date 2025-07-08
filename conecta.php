33120e44ed047bf19b053a5f91eeccd4bcc7ddfd
Sebastião Silva <swillian2009@gmail.com>
2025-07-08 15:55:48 +0000

    Conexão Servidor 103

--- conecta.php
+++ conecta.php
@@ -1,7 +1,7 @@
 <?php
     $server_name = "localhost:3306";
-    $db_username = "root";
-    $db_password = "@Swillian18";
+    $db_username = "northtale";
+    $db_password = "*KingOfYourIllusion12";
     $db_name = "biblioteca";
     
     $connection = mysqli_connect($server_name,$db_username,$db_password,$db_name) or die ('Desconnect');
